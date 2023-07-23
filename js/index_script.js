102676320e6b1f5e139c5ee237b248cb337553c6
theo <55833689+tlombart@users.noreply.github.com>
2023-07-23 20:08:33 +0200

    july update
    + index finished

--- js/index_script.js
+++ js/index_script.js
@@ -43,7 +43,7 @@ search_bar.addEventListener("input", function(e){
     // if we find a result, we format it then display it
     if(results_list.length > 0){
       results_div.classList.remove("no-result")
-      results_div.innerHTML = format_results(results_list)
+      results_div.innerHTML = format_results(results_list).join(' ')
     }
     // else, we make a random word suggestion from all the words in our descriptions
     // and set an event for the user to search this suggestion directly
@@ -75,38 +75,47 @@ while (random_projects.length < nb_of_projects_to_get) {
 }
 random_projects = [... new Set(random_projects)]
 
-console.log(random_projects)
-
 const positions = [['18%', '8%'], ['31%', '72%'], ['24%', '34%']]
+// for each random project selected :
+// => we display the suggestion in the suggestions_ div (add a div for each suggestion)
+// => we open a popup with the linked project
+// => we add an event to the suggested project div in suggestions_
+//    this event opens/closes the project on click
 random_projects.forEach(function(value, index){
   project = Object.values(projects)[value]
   key = Object.keys(projects)[value]
 
-  suggestions_.innerHTML += get_project_html(value, ['search-suggestion'], key)
+  let suggestion_div = get_project_div(value, ['search-suggestion'])
+  suggestions_.appendChild(suggestion_div)
 
   if(project["is_clickable"]){
     display_project_popup(value, positions[index])
+    add_events_for_popup(project)
+
+    // wip for an event to close/open the popup on click on the suggestion_div
+    // this doesn't work for now because only the last suggestion with be listened to
+    // TODO: make the event work for all suggestions
+    // suggestion_div.addEventListener("click", function(e){
+    //   let suggestion_popup = document.getElementById(`popup_project_${key}`)
+    //   let is_popup_open = !!suggestion_popup
+    //   if(is_popup_open) document.body.removeChild(suggestion_popup)
+    //   else display_project_popup(value, positions[index]); add_events_for_popup(project)
+    // })
   }
 })
 
-// after the suggestions popups were opened on pager startup, we can listen to some event, notably closing the popup
-random_projects.forEach(function(value, index){
-  // their is a popup only for clickable projects
-  if(Object.values(projects)[value]["is_clickable"]){
-    let project = Object.values(projects)[value]
+function add_events_for_popup(project){
+  let popup_close_button = document.getElementById(`psha-close_${project.display_name}`)
+  let popup_img = document.getElementById(`img_${project.display_name}`)
 
-    let popup_close_button = document.getElementById(`psha-close_${project.display_name}`)
-    let popup_img = document.getElementById(`img_${project.display_name}`)
-
-    // toggle between open and close
-    popup_close_button.addEventListener("click", function(e){
-      if(popup_close_button.classList.contains('project-sample-header-actions-close')){
-        popup_img.classList.add('hidden')
-        popup_close_button.classList = ['project-sample-header-actions-open']
-      } else {
-        popup_img.classList.remove('hidden')
-        popup_close_button.classList = ['project-sample-header-actions-close']
-      }
-    })
-  }
-})
+  // toggle between open and close
+  popup_close_button.addEventListener("click", function(e){
+    if(popup_close_button.classList.contains('project-sample-header-actions-close')){
+      popup_img.classList.add('hidden')
+      popup_close_button.classList = ['project-sample-header-actions-open']
+    } else {
+      popup_img.classList.remove('hidden')
+      popup_close_button.classList = ['project-sample-header-actions-close']
+    }
+  })
+}
